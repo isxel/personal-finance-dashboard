2db5aea8ed8014a745b5891b34dd5c917accd3c4
isxel <isaelramirez8@gmail.com>
2025-01-06 13:32:28 -0600

    styling changes and changes to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Hero from "./components/Hero.jsx";
 import Banner from "./components/Banner.jsx";
+import Features from "./components/Features.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <>
               <Hero />
               <Banner />
+              <Features />
             </>
           }
         />
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,24 @@
+import style from "../styles/Features.module.css";
+
 function Features() {
-    return (
-        
-    )
+  return (
+    <div className={style.features}>
+      <div className={style.leftSide}>
+        <h1 className={style.titleText}>Comprehensive Dashboard</h1>
+        <h2 className={style.subText}>
+          See all your financial details in one place.
+        </h2>
+        <h1 className={style.titleText}>Interactive Graphs</h1>
+        <h2 className={style.subText}>
+          Visualize your income, spending, and savings trends.
+        </h2>
+        <h1 className={style.titleText}>Transaction History</h1>
+        <h2 className={style.subText}>
+          Easily track and categorize your expenses.
+        </h2>
+      </div>
+    </div>
+  );
 }
+
+export default Features;
