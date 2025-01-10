232cc5490cbb2bd8b95fe7d8a71191d0380cf3b2
Isael <iramir17@stedwards.edu>
2025-01-09 23:13:15 -0600

    Dashboard page now greets based on time of day.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import style from "../styles/Dashboard.module.css";
 import MonthlySpending from "../components/MonthlySpending.jsx";
 import MonthlyIncome from "../components/MonthlyIncome.jsx";
@@ -5,9 +6,21 @@ import MoneySaved from "../components/MoneySaved.jsx";
 import TransactionHistory from "../components/TransactionHistory.jsx";
 
 function Dashboard() {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      setGreeting("Good Morning!");
+    } else if (hour < 18) {
+      setGreeting("Good Afternoon!");
+    } else {
+      setGreeting("Good Evening!");
+    }
+  }, []);
   return (
     <div className={style.dashboard}>
-      <h1 className={style.heading}>Welcome Back, User</h1>
+      <h1 className={style.heading}>{greeting}</h1>
       <div className={style.widgets}>
         <MoneySaved />
         <MonthlySpending />
